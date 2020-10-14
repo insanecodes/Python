a1a6d116e1779d8fcb529783fdc8bcfd7581b83b
Aman Sharma <72183900+AmanSharma703@users.noreply.github.com>
2020-10-15 02:38:19 +0530

    Create Analog Clock using Python-Turtle

---
+++ Source Code/20_analog_clock.py
@@ -0,0 +1,69 @@
+# Create Analog Clock Using Python
+import turtle
+import time
+
+window = turtle.Screen()
+window.bgcolor("black")
+window.setup(width=600, height=600)
+window.title("Analog Clock")
+window.tracer(0)
+
+# Create the drawing pen
+pen = turtle.Turtle()
+pen.hideturtle()
+pen.speed(0)
+pen.pensize(3)
+
+
+def draw_clock(hr, mn, sec, pen):
+
+    # Draw clock face
+    pen.up()
+    pen.goto(0, 210)
+    pen.setheading(180)
+    pen.color("purple")
+    pen.pendown()
+    pen.circle(210)
+
+    # Draw hour hashes
+    pen.up()
+    pen.goto(0, 0)
+    pen.setheading(90)
+
+    for _ in range(12):
+        pen.fd(190)
+        pen.pendown()
+        pen.fd(20)
+        pen.penup()
+        pen.goto(0, 0)
+        pen.rt(30)
+
+    # Draw the hands
+    # Each tuple in list hands describes the color, the length
+    # and the divisor for the angle
+    hands = [("white", 80, 12), ("blue", 150, 60), ("red", 110, 60)]
+    time_set = (hr, mn, sec)
+
+    for hand in hands:
+        time_part = time_set[hands.index(hand)]
+        angle = (time_part/hand[2])*360
+        pen.penup()
+        pen.goto(0, 0)
+        pen.color(hand[0])
+        pen.setheading(90)
+        pen.rt(angle)
+        pen.pendown()
+        pen.fd(hand[1])
+
+
+while True:
+    hr = int(time.strftime("%I"))
+    mn = int(time.strftime("%M"))
+    sec = int(time.strftime("%S"))
+
+    draw_clock(hr, mn, sec, pen)
+    window.update()
+    time.sleep(1)
+    pen.clear()
+
+window.mainloop()
