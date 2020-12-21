e3231dd83819aec5d6de102edc5fdae44ede5af8
Aman Sharma <72183900+AmanSharma703@users.noreply.github.com>
2020-12-22 00:11:12 +0530

    Get System details using python

---
+++ Source Code/28_system_details.py
@@ -0,0 +1,14 @@
+import platform
+
+# displaying OS name 
+print('system   :', platform.system())
+# display node name
+print('node     :', platform.node())
+# display system’s release
+print('release  :', platform.release())
+# display system’s release version
+print('version  :', platform.version())
+# display the machine type
+print('machine  :', platform.machine())
+# display the (real) processor name
+print('processor:', platform.processor())
